a18ae630fb37ec8fbd5d2a0c1b9b49396590ae28
Sean Fernandez <scfernz@gmail.com>
2016-06-23 15:48:39 -0700

    Added AJAX calls with jQuery

---
+++ app/assets/javascripts/animals.js
@@ -0,0 +1,39 @@
+$("document").ready(function() {
+
+  $("#add_new_animal_button").on(
+  "click",
+  function() {
+    // Data to be submitted
+    new_animal = {
+      "animal": {
+        "name": $("#animal_name").val(),
+        "latin_name": $("#animal_latin_name").val(),
+        "kingdom": $("#animal_kingdom").val()
+      }
+    };
+
+    $.ajax({
+      dataType: 'json',
+      url: '/animals',
+      method: 'POST',
+      data: new_animal,
+      success: function(data) {
+        add_to_animal_list(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Add new animal failed: " + errorThrown);
+      }
+    });
+  });// end add wine
+
+  // Function to be called after data has been successfully submitted
+  function add_to_animal_list(data) {
+    alert(JSON.stringify(data));
+    alert("Animal added");
+    $("#animal_list").append(
+      '<tr><td>' + $("#animal_name").val() + '</td><td>' +
+      $("#animal_latin_name").val() + '</td><td>' +
+      $("#animal_kingdom").val() + '</td><td>' + '<a href="/animals/' + data.id + '">Show</a>' + '</td><td>' + '<a href="/animals/' + data.id + '/edit">Edit</a>' + '</td><td>' + '<a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/animals/' + data.id + '">Destroy</a>' + '</td></tr>');
+  }
+
+}); // end document ready
